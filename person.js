55471440ea23bb677398aebbfba0b03d382586e9
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-09-12 00:02:09 -0500

    submitting exercise 6

--- person.js
+++ person.js
@@ -1,5 +1,18 @@
-class Person {
-  // Write code here
-}
-
+// Exercise 6
+// In your Person class, introduce a private field named #id that gets a random value when an object is instantiated.
+// Create a getter method to retrieve this ID value.
+// Instantiate a Person object and try accessing the ID directly and via the getter. 
+  class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+      this._id = Math.random(); 
+    }
+  
+    get getID() {
+      return this._id;
+    }
+  }
+  
+ 
 module.exports = Person;
